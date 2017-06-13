a0118d110c529d8bdaf516d09c979c48128051e7
Simmy <simmy@simmybot.com>
2017-06-13 18:24:42 -0400

    Added A Postive Indicator!

--- classs/coinBaseChange.py
+++ classs/coinBaseChange.py
@@ -8,9 +8,8 @@ class Change:
 
 	def change(self, Crypto, currentPrice, hourlyChange):
 		if hourlyChange < 0.0:
-			#return True
 			return "%s Current Price: %10s | Hourly Change: %s%s%f%s" % (Crypto, currentPrice, Style.BRIGHT, Fore.RED, hourlyChange, Style.RESET_ALL)
 
 		if hourlyChange > 0.0:
-			return "%s Current Price: %10s | Hourly Change: %s%s%f%s" % (Crypto, currentPrice, Style.BRIGHT, Fore.GREEN, hourlyChange, Style.RESET_ALL)
+			return "%s Current Price: %10s | Hourly Change: %s%s+%f%s" % (Crypto, currentPrice, Style.BRIGHT, Fore.GREEN, hourlyChange, Style.RESET_ALL)
 
