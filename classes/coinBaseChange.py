36f1fd00e861880b73b60e8f256357cfca888e0a
Simmy <simmy@simmybot.com>
2017-06-13 22:29:51 -0400

    Brightly Display Crypto

--- classes/coinBaseChange.py
+++ classes/coinBaseChange.py
@@ -8,8 +8,8 @@ class Change:
 
 	def change(self, Crypto, currentPrice, hourlyChange):
 		if hourlyChange < 0.0:
-			return "%s Current Price: %10s | Hourly Change: %s%s%f%s" % (Crypto, currentPrice, Style.BRIGHT, Fore.RED, hourlyChange, Style.RESET_ALL)
+			return "%s%s%s%s Current Price: %10s | Hourly Change: %s%s%f%s" % (Style.BRIGHT, Fore.CYAN, Crypto, Style.RESET_ALL,  currentPrice, Style.BRIGHT, Fore.RED, hourlyChange, Style.RESET_ALL)
 
 		if hourlyChange > 0.0:
-			return "%s Current Price: %10s | Hourly Change: %s%s+%f%s" % (Crypto, currentPrice, Style.BRIGHT, Fore.GREEN, hourlyChange, Style.RESET_ALL)
+			return "%s%s%s%s Current Price: %10s | Hourly Change: %s%s+%f%s" % (Style.BRIGHT, Fore.CYAN, Crypto, Style.RESET_ALL, currentPrice, Style.BRIGHT, Fore.GREEN, hourlyChange, Style.RESET_ALL)
 
